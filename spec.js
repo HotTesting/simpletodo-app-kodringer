721e25e6054d0f20fb5b9fa2f627b8f80a054f88
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-11-09 12:06:34 +0200

    init

---
+++ spec.js
@@ -0,0 +1,8 @@
+//JasmineJS 2
+describe('suite', function () {
+
+    it('test', function () {
+        browser.get('http://maxcabrera.com/code/todo-list/')
+        expect(browser.getTitle()).toBe('Angular2 Seed')
+    })
+})
