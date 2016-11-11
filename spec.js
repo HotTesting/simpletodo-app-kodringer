5482bf63170b5b4e019668cb239ea8c09fbf451d
Alexey Dolmatov <st.link@gmail.com>
2016-11-11 09:55:19 +0200

    10.11.2016

--- spec.js
+++ spec.js
@@ -1,8 +1,24 @@
 //JasmineJS 2
-describe('suite', function () {
 
-    it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
+let URL = 'http://maxcabrera.com/code/todo-list/'
+describe('TODO site', function () {
+
+    it('should have correct title', function () {
+        browser.get(URL)
+//      let title = browser.getTitle()
+//      expect(title).toBe('Angular2 Seed')
         expect(browser.getTitle()).toBe('Angular2 Seed')
+    })
+
+    it('should create new note', function () {
+        browser.get(URL)
+        let webelement = $('input.enter-todo')
+        webelement.sendKeys('Ruby')
+        webelement.submit()
+
+        let notes = $$('todo-list .small-12')
+
+//      expect(notes.getText()).toContain('test note')
+        expect(notes.getText()).toContain('Ruby')
     })
 })
