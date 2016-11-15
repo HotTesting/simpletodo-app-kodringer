8fdb5d65af6369b43a3c1c9c14563442dfde0d36
Alexey Dolmatov <st.link@gmail.com>
2016-11-15 02:04:40 +0200

    hw20161115

--- spec.js
+++ spec.js
@@ -1,6 +1,9 @@
 //JasmineJS 2
 
 let URL = 'http://maxcabrera.com/code/todo-list/'
+let done = $("input[type='checkbox']")
+let list = element.all(by.css("input[type='checkbox']"))
+
 describe('TODO site', function () {
 
     it('should have correct title', function () {
@@ -12,13 +15,25 @@ describe('TODO site', function () {
 
     it('should create new note', function () {
         browser.get(URL)
-        let webelement = $('input.enter-todo')
-        webelement.sendKeys('Ruby')
+    let webelement = $('input.enter-todo')
+        webelement.sendKeys('R')
         webelement.submit()
-
-        let notes = $$('todo-list .small-12')
-
-//      expect(notes.getText()).toContain('test note')
-        expect(notes.getText()).toContain('Ruby')
+    let notes = $$('todo-list .small-12')
+        expect(notes.getText()).toContain('R')
     })
+
+    it('should delete note', function (){
+        browser.get(URL)
+        element((done).click())
+    let newlist = element.all(by.css("input[type='checkbox']"))
+        expect(list.count() != newlist.count()).toBe(true)
+    })
+    it('should delete all notes', function(){
+        browser.get(URL)
+//      list.each(function () { element(done).click() })
+        list.then(function (del){
+        for (let i = 0; i < del.length; i++) {del[i].click()}})
+        expect(list.count()).toBe(0)
+    })
+
 })
